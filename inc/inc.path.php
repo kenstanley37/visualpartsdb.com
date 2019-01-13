d582b32b44464b96dfc8fb67e01bb7e9978f8336
Kenneth Stanley <ken@stanleysoft.org>
2019-01-13 18:43:42 -0500

    Update inc.path.php
    fixed a path issue

--- inc/inc.path.php
+++ inc/inc.path.php
@@ -3,5 +3,5 @@
 define('ROOT_DIR', dirname(__FILE__));
 
 //remove the \inc from the ROOT_DIR
-$path = str_replace('\inc', DIRECTORY_SEPARATOR, ROOT_DIR);
+$path = str_replace('inc', DIRECTORY_SEPARATOR, ROOT_DIR);
 ?>
