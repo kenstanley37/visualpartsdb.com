4bbd07caadb3974ca973e87aed52468545eb2f03
Kenneth Stanley <ken@stanleysoft.org>
2019-01-16 22:04:51 -0500

    bug fix

---
+++ inc/inc.navbar.php
@@ -0,0 +1,12 @@
+<section class="navPartManagement">
+    <a href="#" class="navLinks">Add Image</a>
+    <a href="#" class="navLinks">Edit Data</a>
+    <a href="#" class="navLinks">New Part</a>
+</section>
+<section class="navOther">
+    <a href="#" class="navLinks">Most Searched</a>
+</section>
+<section class="navUserManagement">
+    <a href="#" class="navLinks">User Management</a>
+    <a href="#" class="navLinks">Analytics</a>
+</section>
