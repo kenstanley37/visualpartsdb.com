d001de4aecabd3edde5cb946abbf562812070d32
Kenneth Stanley <ken@stanleysoft.org>
2019-01-24 08:50:54 -0500

    Update class.user.php

--- class/class.user.php
+++ class/class.user.php
@@ -157,7 +157,7 @@
                     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
                     $mail->isSMTP();                                      // Set mailer to use SMTP
                     $mail->Host = 'visualpartsdb.com';  // Specify main and backup SMTP servers
-                    $mail->SMTPAuth = false;                               // Enable SMTP authentication
+                    //$mail->SMTPAuth = true;                               // Enable SMTP authentication
                     //$mail->Username = 'user@example.com';                 // SMTP username
                     //$mail->Password = 'secret';                           // SMTP password
                     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
