a0fcf8dda95e16f843a6e44e1d83102c40d95e2a
Kenneth Stanley <ken@stanleysoft.org>
2019-03-27 17:06:33 -0400

    phpmailer turn off debug

--- class/class.user.php
+++ class/class.user.php
@@ -228,7 +228,7 @@
                     
                     
                     //Server settings
-                    $mail->SMTPDebug = 2;                           // Enable verbose debug output
+                    $mail->SMTPDebug = 0;                           // Enable verbose debug output
                     $mail->isSMTP();                                // Set mailer to use SMTP
                     $mail->Host = 'visualpartsdb.com';  // Specify main and backup SMTP servers
                     $mail->SMTPAuth = true;                         // Enable SMTP authentication
