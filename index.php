ceb4d888d14f13f8ce866af98ffeb56e4a8099e5
Kenneth Stanley <ken@stanleysoft.org>
2019-04-10 19:11:49 -0400

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if(isset($_POST['RegisterRequest']))
     // Make and decode POST request:
     $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
     $recaptcha = json_decode($recaptcha);
-    echo $recaptcha;
+    print_r($recaptcha);
 
     // Take action based on the score returned:
     if ($recaptcha->score >= 0.5) {
