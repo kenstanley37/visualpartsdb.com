a7e52f5bd974747ae01c3b301018092f55c1a1e5
Kenneth Stanley <ken@stanleysoft.org>
2019-04-14 17:20:30 -0400

    Update inc.head.php

--- inc/inc.head.php
+++ inc/inc.head.php
@@ -40,7 +40,7 @@
     <!-- CSS MENU -->
     <link rel="stylesheet" type="text/css" media="screen" href="/inc/cssmenu/cssmenu.css" />
     <!-- DataTables -->
-    <link rel="stylesheet" type="text/css" href="/vendor/DataTables/dataTables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/ju/jq-3.3.1/dt-1.10.18/af-2.3.3/r-2.2.2/datatables.min.css"/>
     <link rel="stylesheet" type="text/css" href="/vendor/DataTables/responsive.dataTables.min.css"/>
     <!-- VisualPartsDB CSS -->
     <link rel="stylesheet" type="text/css" media="screen" href="/inc/css/visualpartsdb.css" />
@@ -50,7 +50,7 @@
     <script src="/inc/js/jquery-3.3.1.min.js"></script>
     <script src="/vendor/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
     <!-- DataTables -->
-    <script src="../vendor/DataTables/dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/ju/jq-3.3.1/dt-1.10.18/af-2.3.3/r-2.2.2/datatables.min.js"></script>
     <script src="/vendor/DataTables/Responsive-2.2.2/dataTables.responsive.min.js"></script>
     <!-- Graphs and Charts -->
     <script src="/vendor/d3/d3.min.js"></script>
