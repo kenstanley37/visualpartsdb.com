542d045458b7b5d1874f02fc2d145e069e1465fd
Kenneth Stanley <ken@stanleysoft.org>
2019-04-29 08:38:36 -0400

    fixes

--- class/class.user.php
+++ class/class.user.php
@@ -249,7 +249,7 @@
                     $mail->Host = 'visualpartsdb.com';  // Specify main and backup SMTP servers
                     $mail->SMTPAuth = true;                         // Enable SMTP authentication
                     $mail->Username = 'register@visualpartsdb.com';     // SMTP username
-                    $mail->Password = 'on55f}tkd[k(';                           // SMTP password
+                    $mail->Password = 'gDHHM(z;pq9K';                           // SMTP password
                     $mail->SMTPSecure = 'ssl';                      // Enable TLS encryption, `ssl` also accepted
                     $mail->Port = 465;                              // TCP port to connect to
 
